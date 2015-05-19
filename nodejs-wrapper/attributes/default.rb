8d458db6ba0a546fd198f25ea2475ac9239d9dde
BlueHotDog <dannifriedland@gmail.com>
2015-05-18 19:46:37 -0700

    more work

--- nodejs-wrapper/attributes/default.rb
+++ nodejs-wrapper/attributes/default.rb
@@ -4,4 +4,4 @@ default['nodejs']['version'] = '0.12.0'
 default['nodejs']['install_method'] = 'binary'
 
 default["nodebin"]["location"] = '/usr/local/nodejs-binary/bin'
-default["nodebin"]["opsworks_location"] = '/usr/local/bin/node'
+default["nodebin"]["opsworks_location"] = '/usr/local/bin'
