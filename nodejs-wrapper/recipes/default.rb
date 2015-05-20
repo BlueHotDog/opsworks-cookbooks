4066c575a1cab5cb7f134f53c1a1ee913ceb8e49
BlueHotDog <dannifriedland@gmail.com>
2015-05-19 17:58:32 -0700

    initial

---
+++ nodejs-wrapper/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: nodejs-0.12.0-wrapper
+# Recipe:: default
+#
+# Copyright (C) 2015 Dimitar Pavlov
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'nodejs'
+include_recipe 'nodejs::nodejs_from_binary'
