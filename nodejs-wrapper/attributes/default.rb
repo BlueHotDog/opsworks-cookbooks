a973795d50c0afdfa69e257421c860838f5fcbef
BlueHotDog <dannifriedland@gmail.com>
2015-05-19 20:16:20 -0700

    more work

--- nodejs-wrapper/attributes/default.rb
+++ nodejs-wrapper/attributes/default.rb
@@ -3,5 +3,7 @@
 default['nodejs']['version'] = '0.12.0'
 default['nodejs']['install_method'] = 'binary'
 
-default["nodebin"]["location"] = '/usr/local/nodejs-binary/bin'
-default["nodebin"]["opsworks_location"] = '/usr/local/bin'
+default['nodebin']['location'] = '/usr/local/nodejs-binary/bin'
+default['nodebin']['opsworks_location'] = '/usr/local/bin'
+
+default['nginx']['passenger']['nodejs'] = "#{default['nodebin']['opsworks_location']}/node"
--- nodejs-wrapper/metadata.rb
+++ nodejs-wrapper/metadata.rb
@@ -7,3 +7,4 @@ long_description 'Installs/Configures nodejs-0.12.0'
 version          '0.1.0'
 
 depends 'nodejs'
+depends 'nginx'
