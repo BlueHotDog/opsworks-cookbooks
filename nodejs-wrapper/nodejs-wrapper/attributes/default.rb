dce66dc20d814283a7c7a4ad04749b30669e6074
BlueHotDog <dannifriedland@gmail.com>
2015-05-19 18:00:32 -0700

    more work

--- nodejs-wrapper/nodejs-wrapper/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-
-# Override node the version
-default['nodejs']['version'] = '0.12.0'
-default['nodejs']['install_method'] = 'binary'
-
-default["nodebin"]["location"] = '/usr/local/nodejs-binary/bin'
-default["nodebin"]["opsworks_location"] = '/usr/local/bin'
--- nodejs-wrapper/recipes/default.rb
+++ nodejs-wrapper/recipes/default.rb
@@ -9,3 +9,4 @@
 
 include_recipe 'nodejs'
 include_recipe 'nodejs::nodejs_from_binary'
+include_recipe 'nodejs::create-symlink'
