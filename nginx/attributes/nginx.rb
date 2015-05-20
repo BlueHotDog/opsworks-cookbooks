6c070ddedead35edbc3733a8720aa495750cc56d
BlueHotDog <dannifriedland@gmail.com>
2015-05-19 20:45:56 -0700

    more work

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -15,24 +15,24 @@
 ###
 
 case node[:platform_family]
-when "debian"
-  default[:nginx][:dir]        = "/etc/nginx"
-  default[:nginx][:log_dir]    = "/var/log/nginx"
-  default[:nginx][:user]       = "www-data"
-  default[:nginx][:binary]     = "/usr/sbin/nginx"
-  if node[:platform_version] == "14.04"
-    default[:nginx][:pid_file] = "/run/nginx.pid"
-  else
+  when "debian"
+    default[:nginx][:dir] = "/etc/nginx"
+    default[:nginx][:log_dir] = "/var/log/nginx"
+    default[:nginx][:user] = "www-data"
+    default[:nginx][:binary] = "/usr/sbin/nginx"
+    if node[:platform_version] == "14.04"
+      default[:nginx][:pid_file] = "/run/nginx.pid"
+    else
+      default[:nginx][:pid_file] = "/var/run/nginx.pid"
+    end
+  when "rhel"
+    default[:nginx][:dir] = "/etc/nginx"
+    default[:nginx][:log_dir] = "/var/log/nginx"
+    default[:nginx][:user] = "nginx"
+    default[:nginx][:binary] = "/usr/sbin/nginx"
     default[:nginx][:pid_file] = "/var/run/nginx.pid"
-  end
-when "rhel"
-  default[:nginx][:dir]        = "/etc/nginx"
-  default[:nginx][:log_dir]    = "/var/log/nginx"
-  default[:nginx][:user]       = "nginx"
-  default[:nginx][:binary]     = "/usr/sbin/nginx"
-  default[:nginx][:pid_file]   = "/var/run/nginx.pid"
-else
-  Chef::Log.error "Cannot configure nginx, platform unknown"
+  else
+    Chef::Log.error "Cannot configure nginx, platform unknown"
 end
 
 default[:nginx][:log_format] = {}
--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-package "nginx"
+# package "nginx"
 
 directory node[:nginx][:dir] do
   owner 'root'
